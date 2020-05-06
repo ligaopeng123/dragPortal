ed7f815ba1d64655bbd6bb7f527a34c95e8d00c7
ligaoming <1170234932@qq.com>
2020-05-06 13:16:46 +0800

    合入原因：拖拽功能实现 合入时间：2020-5-6 合入责任人：pgli

--- drag/event.js
+++ drag/event.js
@@ -11,79 +11,78 @@
  */
 
 export const DragEvent = (function () {
-  return {
-    /**
-     * 事件绑定 有一些兼容性处理
-     * @param dom
-     * @param event
-     * @param fn
-     * @param useCapture
-     */
-    addEventListener: (dom, event, fn, useCapture = false) => {
-      if (dom.addEventListener) {
-        dom.addEventListener(event, fn, useCapture);
-      } else if (dom.attachEvent) {
-        dom.attachEvent(`on${event}`, fn);
-      } else {
-        dom[`on${event}`] = fn;
-      }
-    },
-    /**
-     * 添加class样式
-     * @param el
-     * @param className
-     */
-    addClass(el, className) {
-      el.className = el.className + ' ' + className;
-    },
-    /**
-     * 移除样式
-     * @param el
-     * @param className
-     */
-    removeClass(el, className) {
-      el.className = (el.className || '').replace(className, '');
-    },
-    /**
-     * 设置attr属性
-     * @param el
-     * @param attr
-     * @param attrVal
-     */
-    setAttribute(el, attr, attrVal) {
-      el.setAttribute(attr, attrVal);
-    },
-    /**
-     * 再之后插入
-     * @param newElement
-     * @param targetElement
-     */
-    insertAfter(newElement, targetElement) {
-      const parent = targetElement.parentNode;
-      //如果要插入的目标元素是其父元素的最后一个元素节点，直接插入该元素
-      //否则，在目标元素的下一个兄弟元素之前插入
-      if (parent.lastChild == targetElement) {
-        parent.appendChild(targetElement);
-      } else {
-        parent.insertBefore(newElement, targetElement.nextSibling);
-      }
-    },
-    /**
-     *
-     * @param node 父元素
-     * @param newnode  要插入的节点对象
-     * @param existingnode  要添加新的节点前的子节点。
-     */
-    insertBefore(node, newnode, existingnode) {
-      node.insertBefore(newnode,existingnode);
-    },
-    /**
-     * 移除元素
-     * @param node  父元素
-     * @param childNode  子元素
-     */
-    removeChild(node, childNode) {
-      node.removeChild(childNode);
+    return {
+        /**
+         * 事件绑定 有一些兼容性处理
+         * @param dom
+         * @param event
+         * @param fn
+         * @param useCapture
+         */
+        addEventListener: (dom, event, fn, useCapture = false) => {
+            if (dom.addEventListener) {
+                dom.addEventListener(event, fn, useCapture);
+            } else if (dom.attachEvent) {
+                dom.attachEvent(`on${event}`, fn);
+            } else {
+                dom[`on${event}`] = fn;
+            }
+        },
+        /**
+         * 添加class样式
+         * @param el
+         * @param className
+         */
+        addClass(el, className) {
+            el.className = el.className + ' ' + className;
+        },
+        /**
+         * 移除样式
+         * @param el
+         * @param className
+         */
+        removeClass(el, className) {
+            el.className = (el.className || '').replace(className, '');
+        },
+        /**
+         * 设置attr属性
+         * @param el
+         * @param attr
+         * @param attrVal
+         */
+        setAttribute(el, attr, attrVal) {
+            el.setAttribute(attr, attrVal);
+        },
+        /**
+         * 再之后插入
+         * @param newElement
+         * @param targetElement
+         */
+        insertAfter(node, newElement, targetElement) {
+            //如果要插入的目标元素是其父元素的最后一个元素节点，直接插入该元素
+            //否则，在目标元素的下一个兄弟元素之前插入
+            if (node.lastChild === targetElement) {
+                node.appendChild(targetElement);
+            } else {
+                node.insertBefore(newElement, targetElement.nextSibling);
+            }
+        },
+        /**
+         *
+         * @param node 父元素
+         * @param newnode  要插入的节点对象
+         * @param existingnode  要添加新的节点前的子节点。
+         */
+        insertBefore(node, newnode, existingnode) {
+            node.insertBefore(newnode, existingnode);
+        },
+        /**
+         * 移除元素
+         * @param node  父元素
+         * @param childNode  子元素
+         */
+        removeChild(node, childNode) {
+            node.removeChild(childNode);
+        }
     }
-  }
 })();
--- drag/portal.js
+++ drag/portal.js
@@ -17,82 +17,82 @@ import {DragAction} from './dragAction.js'
 
 
 export const Portal = (function (dom) {
-  /**
-   * id允许传入字符串 或者dom类型
-   * 如果是dom类型 则直接使用
-   * 如果是字符串类型 则去查询下是否有该节点 如果有则使用 没有则创建
-   * @type {*|Element}
-   * @private
-   */
-  const _portal = DragUtils.isString(dom) ? (elUtils.getElementById(dom) ? elUtils.getElementById(dom) : elUtils.createElement('div')) : dom;
-  /**
-   * 添加className
-   * @type {string}
-   */
-  DragEvent.addClass(_portal, 'portal');
-  /**
-   * 拖拽过程中的样式
-   * @type {string}
-   */
-  const selectClass = 'portal-item-dragSelectd';
-  /**
-   * 根拖拽元素样式
-   * @type {string}
-   */
-  const itemClass = 'portal-item-size';
-  const Portal = {
     /**
-     * 保存portal对象 后期放一些属性 获取方便对外暴露一些函数
+     * id允许传入字符串 或者dom类型
+     * 如果是dom类型 则直接使用
+     * 如果是字符串类型 则去查询下是否有该节点 如果有则使用 没有则创建
+     * @type {*|Element}
+     * @private
      */
-    _portal: _portal,
+    const _portal = DragUtils.isString(dom) ? (elUtils.getElementById(dom) ? elUtils.getElementById(dom) : elUtils.createElement('div')) : dom;
     /**
-     * 拖拽的具体位置 到了谁的位置
+     * 添加className
+     * @type {string}
      */
-    __nextPortal: null,
+    DragEvent.addClass(_portal, 'portal');
     /**
-     * 初始化容器 根据数据拼接相对应的dom
-     * @param data
+     * 拖拽过程中的样式
+     * @type {string}
      */
+    const selectClass = 'portal-item-dragSelectd';
     /**
-     * 初始拖拽元素
+     * 根拖拽元素样式
+     * @type {string}
      */
-    _startItem: null,
-    init: function (data) {
-      /**
-       * 创建虚拟dom节点
-       * @type {DocumentFragment}
-       */
-      const innerHTML = document.createDocumentFragment();
-      data.forEach(item => {
-        innerHTML.appendChild(this.createItem(item));
-      });
-      /**
-       * 添加到文档中
-       */
-      this._portal.appendChild(innerHTML);
-    },
-    /**
-     * 开始拖拽
-     * @param e
-     */
-    ondragstart(e) {
-      DragEvent.addClass(this.getSrcElement(e), selectClass);
-    },
-    /**
-     * 执行item回调函数
-     * @param item
-     */
-    executeItemFn(item, key) {
-      DragUtils.isFunction(item[key]) ? item[key](e, item) : '';
-    },
-    /**
-     * 创建每一个拖拽组件容器
-     * @param item
-     * @returns {string}
-     */
-    createItem(item) {
-      const itemPortal = this.getItemPortal(item);
-      itemPortal.innerHTML = `
+    const itemClass = 'portal-item-size';
+    const Portal = {
+        /**
+         * 保存portal对象 后期放一些属性 获取方便对外暴露一些函数
+         */
+        _portal: _portal,
+        /**
+         * 拖拽的具体位置 到了谁的位置
+         */
+        __nextPortal: null,
+        /**
+         * 初始化容器 根据数据拼接相对应的dom
+         * @param data
+         */
+        /**
+         * 初始拖拽元素
+         */
+        _startItem: null,
+        init: function (data) {
+            /**
+             * 创建虚拟dom节点
+             * @type {DocumentFragment}
+             */
+            const innerHTML = document.createDocumentFragment();
+            data.forEach(item => {
+                innerHTML.appendChild(this.createItem(item));
+            });
+            /**
+             * 添加到文档中
+             */
+            this._portal.appendChild(innerHTML);
+        },
+        /**
+         * 开始拖拽
+         * @param e
+         */
+        ondragstart(e) {
+            DragEvent.addClass(this.getSrcElement(e), selectClass);
+        },
+        /**
+         * 执行item回调函数
+         * @param item
+         */
+        executeItemFn(item, key) {
+            DragUtils.isFunction(item[key]) ? item[key](e, item) : '';
+        },
+        /**
+         * 创建每一个拖拽组件容器
+         * @param item
+         * @returns {string}
+         */
+        createItem(item) {
+            const itemPortal = this.getItemPortal(item);
+            itemPortal.innerHTML = `
         <div class="portal-item">
           <div class="portal-item-top"></div>
           <div class="portal-item-title">${item.title}</div>
@@ -102,93 +102,98 @@ export const Portal = (function (dom) {
           <div class="portal-item-right"></div>
         </div>
       `;
-      return itemPortal;
-    },
-    /**
-     * 获取ItemPortal 容器
-     * @returns {*}
-     */
-    getItemPortal(item) {
-      // 创建一个拖拽容器
-      const itemPortal = elUtils.createElement('div');
-      /**
-       * 给容器添加样式
-       */
-      DragEvent.addClass(itemPortal, itemClass);
-      /**
-       * 给容器设置属性draggable
-       */
-      DragEvent.setAttribute(itemPortal, 'draggable', true);
-      /**
-       * 设置拖拽事件
-       */
-      this.setItemPortalEvent(itemPortal, item);
-      return itemPortal;
-    },
-    /**
-     * 设置拖拽事件
-     * @param item
-     */
-    setItemPortalEvent(itemPortal, item) {
-      DragAction({itemPortal})
-        .ondragstart((e) => {
-          this.ondragstart(e, item);
-          this.executeItemFn(item, ondragstart);
-          /**
-           * 记录拖拽元素
-           */
-          this._startItem = this.getItemBySrcElement(this.getSrcElement(e));
-        })
-        .ondrag((e) => {
-          this.executeItemFn(item, ondrag);
-        })
-        .ondragend((e) => {
-          DragEvent.removeClass(this.getSrcElement(e), selectClass);
-          this.executeItemFn(item, ondragend);
-        })
-        // .ondragenter((e) => {
-        //   this.executeItemFn(item, ondragenter);
-        // })
-        .ondragover((e) => {
-          this.executeItemFn(item, ondragover);
-        })
-        // .ondragleave((e) => {
-        //   this.executeItemFn(item, ondragleave);
-        // })
-        .ondrop((e) => {
-          /**
-           * 获取拖拽放下元素
-           */
-          const last = this.getItemBySrcElement(this.getSrcElement(e));
-          /**
-           * 仅当 拖拽元素和放下元素不一致 并且 拖拽距离足够时 才触发拖拽响应
-           */
-          if (this._startItem !== last) {
-            DragEvent.removeChild(this._portal, this._startItem);
-            DragEvent.insertBefore(this._portal, this._startItem, last);
-          }
-          this._startItem = null;
-          this.executeItemFn(item, ondrop);
-        });
-    },
-    /**
-     * 根据srcElement 或者相对应的根元素
-     * @param srcElement
-     * @returns {*}
-     */
-    getItemBySrcElement(srcElement) {
-      const className = srcElement.className;
-      if (className.includes(itemClass)) return srcElement;
-      return this.getItemBySrcElement(srcElement.parentNode)
-    },
-    /**
-     * 获取srcElement元素
-     * @param event
-     * @returns {Object|event.target|{get}|any}
-     */
-    getSrcElement(event) {
-      return elUtils.getSrcElement(event);
-    }
-  };
-  return Portal;
+            return itemPortal;
+        },
+        /**
+         * 获取ItemPortal 容器
+         * @returns {*}
+         */
+        getItemPortal(item) {
+            // 创建一个拖拽容器
+            const itemPortal = elUtils.createElement('div');
+            /**
+             * 给容器添加样式
+             */
+            DragEvent.addClass(itemPortal, itemClass);
+            /**
+             * 给容器设置属性draggable
+             */
+            DragEvent.setAttribute(itemPortal, 'draggable', true);
+            /**
+             * 设置拖拽事件
+             */
+            this.setItemPortalEvent(itemPortal, item);
+            return itemPortal;
+        },
+        /**
+         * 设置拖拽事件
+         * @param item
+         */
+        setItemPortalEvent(itemPortal, item) {
+            DragAction({itemPortal})
+                .ondragstart((e) => {
+                    this.ondragstart(e, item);
+                    /**
+                     * 记录拖拽元素
+                     */
+                    this._startItem = this.getItemBySrcElement(this.getSrcElement(e));
+                    this.executeItemFn(item, ondragstart);
+                })
+                .ondrag((e) => {
+                    this.executeItemFn(item, ondrag);
+                })
+                .ondragend((e) => {
+                    DragEvent.removeClass(this.getSrcElement(e), selectClass);
+                    this.executeItemFn(item, ondragend);
+                })
+                // .ondragenter((e) => {
+                //   this.executeItemFn(item, ondragenter);
+                // })
+                .ondragover((e) => {
+                    this.executeItemFn(item, ondragover);
+                })
+                // .ondragleave((e) => {
+                //   this.executeItemFn(item, ondragleave);
+                // })
+                .ondrop((e) => {
+                    /**
+                     * 获取拖拽放下元素
+                     */
+                    const last = this.getItemBySrcElement(this.getSrcElement(e));
+                    /**
+                     * 仅当 拖拽元素和放下元素不一致 并且 拖拽距离足够时 才触发拖拽响应
+                     */
+                    if (this._startItem !== last) {
+                        if (e.offsetX * 2 >= last.offsetWidth) {
+                            DragEvent.removeChild(this._portal, this._startItem);
+                            DragEvent.insertAfter(this._portal, this._startItem, last);
+                        } else {
+                            DragEvent.removeChild(this._portal, this._startItem);
+                            DragEvent.insertBefore(this._portal, this._startItem, last);
+                        }
+                    }
+                    this._startItem = null;
+                    this.executeItemFn(item, ondrop);
+                });
+        },
+        /**
+         * 根据srcElement 或者相对应的根元素
+         * @param srcElement
+         * @returns {*}
+         */
+        getItemBySrcElement(srcElement) {
+            const className = srcElement.className;
+            if (className.includes(itemClass)) return srcElement;
+            return this.getItemBySrcElement(srcElement.parentNode)
+        },
+        /**
+         * 获取srcElement元素
+         * @param event
+         * @returns {Object|event.target|{get}|any}
+         */
+        getSrcElement(event) {
+            return elUtils.getSrcElement(event);
+        }
+    };
+    return Portal;
 });
